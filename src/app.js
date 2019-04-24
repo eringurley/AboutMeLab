48a18fe2a283fb81901ba6976e94ae16d6dbf3fa
Erin Gurley <erinmgurley@gmail.com>
2019-04-23 22:45:29 -0700

    played with app.js document to see if i could get error messages cleared

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import quizScore from './quizScore.js';
-const submitButton = document.getElementById('submitButton');
+const submitButton = document.getElementById('submit');
 
 const likeOne = document.getElementById('likeOne');
 const likeTwo = document.getElementById('likeTwo');
@@ -14,22 +14,20 @@ submitButton.addEventListener('click', () => {
     const dislikeTwo = dislikeTwo.value;
     const dislikeThree = dislikeThree.value;
 
+    let score = 0;
+
+    let quizScore = document.getElementById('score');
+
     if(likeOne.trim() === '' || likeTwo.trim() === '' || dislikeOne.trim() === '' || dislikeTwo.trim() === '' || dislikeThree.trim() === '') {
-        alert = 'Please complete all answers.';
-        return;
-    }
-    
-    const quizScore = quizScore(likeOne, likeTwo, dislikeOne, dislikeTwo, dislikeThree);
-    
-
-
-    if(score <2) {
+        quizScore.textConent = 'Please complete all answers.';
+    } else {
+        score = quizScore(likeOne, likeTwo, dislikeOne, dislikeTwo, dislikeThree);
+        if(score <= 2) {
         quizResult.classList.add('bad');
         quizResult.classList.remove('good');
-    }
-    else {
+    }else {
         quizResult.classList.add('good');
         quizResult.classList.remove('bad');
-
+    }
     quizResult.textContent = 'Your score: ' + score + ' Your percentage: ' + (score / 5) + '%';
-    }); 
+    });
